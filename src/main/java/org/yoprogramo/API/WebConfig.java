9928b0dfee696124f5d6a4c32c0d08d4fe97a951
blanche-a <109881004+blanche-a@users.noreply.github.com>
2023-01-15 19:37:08 -0300

    Ajuste AllowedOrigins

--- src/main/java/org/yoprogramo/API/WebConfig.java
+++ src/main/java/org/yoprogramo/API/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://springboot-blanche-a.koyeb.app");
+        registry.addMapping("/**").allowedOrigins("https://registered-jenn-blanche-a.koyeb.app");
     }
+    
 }
