5dc80deff8f9b43b0d89f2c107fb9a16c05879a6
blanche-a <109881004+blanche-a@users.noreply.github.com>
2023-01-15 16:57:36 -0300

    Añado @Override

--- src/main/java/org/yoprogramo/API/WebConfig.java
+++ src/main/java/org/yoprogramo/API/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     
+    @Override
     public void addCorsMapping(CorsRegistry registry) {
         registry.addMapping("/**");
     }
